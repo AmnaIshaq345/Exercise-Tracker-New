a5a0ff44e5d865d3f03e00752757776ace0253e6
AmnaIshaq345 <AmnaIshaq345.github.io>
2021-11-02 23:00:45 +0500

    first commit

---
+++ src/Components/Main_Wrapper.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Main_Wrapper = () => {
+    return (
+        <>
+        <div
+      id="main-wrapper"
+      data-layout="vertical"
+      data-navbarbg="skin5"
+      data-sidebartype="full"
+      data-sidebar-position="absolute"
+      data-header-position="absolute"
+      data-boxed-layout="full"
+    ></div>
+        </>
+    )
+}
+
+export default Main_Wrapper
---
+++ src/Components/Preloader.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const Preloader = () => {
+    return (
+        <>
+        
+        {/* <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== --> */}
+        <div className="preloader">
+            <div className="lds-ripple">
+                <div className="lds-pos"></div>
+                <div className="lds-pos"></div>
+            </div>
+        </div>
+        {/* <!-- ============================================================== -->
+        <!-- Preloader - style you can find in spinners.css -->
+        <!-- ============================================================== -->
+       */}
+        
+      
+        </>
+    )
+}
